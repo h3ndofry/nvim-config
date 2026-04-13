b027b57a6ab2ab160805bee5e9cbbc3b32d6b9b2
Daniel Henderson <daniel.t.henderson@gmail.com>
2026-04-13 17:12:16 +1000

    add blink

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,10 @@
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<CR>"] = {}, -- disable Enter for completion
+      ["<Tab>"] = { 'select_and_accept', 'fallback' }, -- fall back to tab
+    },
+  },
+}
