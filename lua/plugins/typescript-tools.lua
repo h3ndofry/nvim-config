36548d8a90e9e50f74fbe85f0d35551b14d13422
Daniel Henderson <daniel.t.henderson@gmail.com>
2026-04-14 17:30:55 +1000

    fix: adjust config for nx monorepos

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -4,9 +4,16 @@ return {
   opts = {
     settings = {
       -- This helps specifically with Nx path resolution
+      publish_diagnostic_on = "insert_leave",
       expose_as_code_action = "all",
+      -- Important for large monorepos
       tsserver_max_memory = 8192,
-      separate_diagnostic_server = true, -- Runs diagnostics on a separate thread
+      -- Runs diagnostics on a separate thread
+      separate_diagnostic_server = true,
+      -- Explicitly ignore Nx cache and build folders
+      tsserver_file_preferences = {
+        excludeDirectories = { ".nx", "dist", "node_modules" },
+      },
     },
   },
 }
